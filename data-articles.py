b9c52a3c942d81b5dd36090f9216a9de6dd1b31c
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-05 22:49:32 -0400

    Create data-articles.py

---
+++ data-articles.py
@@ -0,0 +1,19 @@
+def Articles(): 
+    articles = [
+        { 
+            'id': 1,
+            'title': 'Article One',
+            'body': 'bla bla bla.',
+            'author': 'Hrithik Vemparala',
+            'create_date': '06-05-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'to be continued.',
+            'author': 'Hrithik Vemparala',
+            'create_date': '06-05-2019'
+        },
+    ]
+
+    return articles
